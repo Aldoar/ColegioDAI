b76dfacfb6ee87bc3b02448fa0fdbfc3e1177518
saezmarcos <saezmarcos@gmail.com>
2017-07-01 22:00:59 -0400

    ColegioDAI
    con inspctor

---
+++ resources/views/nav/inspector.blade.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Colegio DAI</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li><a href="#">Operaciones : </a></li>
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Alumno
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Listar  Alumnos</a></li>
+          <li><a href="#">Consultar Alumno</a></li>
+          <li><a href="#">Registrar Alumno</a></li>
+          <li><a href="#">Eliminar Alumno</a></li>
+        </ul>
+      </li>
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Profesor
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Listar  Profesores</a></li>
+          <li><a href="#">Consultar Profesor</a></li>
+          <li><a href="#">Contratar Profesor</a></li>
+          <li><a href="#">Despedir Profesor</a></li>
+        </ul>
+      </li>
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">Usuario
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Listar  Usuarios</a></li>
+          <li><a href="#">Consultar Usuario</a></li>
+          <li><a href="#">Registrar Usuario</a></li>
+          <li><a href="#">Eliminar Usuario</a></li>
+        </ul>
+      </li>
+    </ul>
+    <ul class="nav navbar-nav navbar-right">
+    	<li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Salir</a></li>
+    </ul>
+  </div>
+</nav>
