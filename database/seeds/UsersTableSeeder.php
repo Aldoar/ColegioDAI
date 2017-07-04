9c4976443bf98bc5d352a13b60335c274fa2d75a
Aldoar <aldo.castagnoli@gmail.com>
2017-07-03 20:04:11 -0400

    nuevos user

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,11 +11,40 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+           
         DB::table('users')->insert([
-            'name' => 'alumno Prueba1',
-            'email' => 'alumno1@tester.cl',
+            'name' => 'Director',
+            'email' => 'director@tester.cl',
+            'password' => bcrypt('1234'),
+            'idRols'=> 1,
+            'rut' => 13072627
+        ]);
+        DB::table('users')->insert([
+            'name' => 'inspector',
+            'email' => 'inspector@tester.cl',
+            'password' => bcrypt('1234'),
+            'idRols'=> 2,
+            'rut' => 13072627
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Secretaria',
+            'email' => 'secretaria@tester.cl',
+            'password' => bcrypt('1234'),
+            'idRols'=> 3,
+            'rut' => 13072627
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Alumno',
+            'email' => 'alumno@tester.cl',
             'password' => bcrypt('1234'),
             'idRols'=> 4,
+            'rut' => 13072627
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Profesor',
+            'email' => 'profesor@tester.cl',
+            'password' => bcrypt('1234'),
+            'idRols'=> 5,
             'rut' => 13072627
         ]);
     }
